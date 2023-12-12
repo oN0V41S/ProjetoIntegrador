cc44b5b59db40c3253f9a6c069ea02d36f11efb7
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-12-12 09:24:04 -0300

    Add files via upload

--- src/assets/home/projetointegrador.js
+++ src/assets/home/projetointegrador.js
@@ -2,11 +2,11 @@ import "../../css/teste.css";
 
 export default function ProjetoIntegrador() {
   return (
-    <section id="#pIntegrador" className="w-10/12 m-auto bg-card1 rounded-nm grid mt-10">
+    <section id="#pIntegrador" className="w-10/12 m-auto bg-card1 rounded-nm grid mt-10 mb-16">
       <h1 className="w-max ml-auto mr-auto text-2xl md:text-4xl mt-4 mb-4">
         Projeto Integrador
       </h1>
-      <div className="w-7/12 ml-8 p-4 rounded-nm min-h-[20vw] grid gap-0 grid-cols-2">
+      <div className="w-4/12 ml-8 p-4 rounded-nm min-h-[20vw] grid gap-20  md:gap-0 grid-cols-2">
         <input
           id="btnHare"
           type="radio"
@@ -23,11 +23,14 @@ export default function ProjetoIntegrador() {
         />
         <label for="btnSop" className="h-max w-max bg-card2 p-2 3 rounded-md">SOP</label>
 
-        <div id="sop" className="hidden col-span-2">
+        <div id="sop" className="hidden col-span-2 mt-4">
           Texto SOP
+          <video src="https://example.com/video.mp4" controls autoplay loop className="w-full">
+            Seu navegador não suporta a reprodução de vídeos.
+          </video>
         </div>
 
-        <div id="hare" className="hidden col-span-2">
+        <div id="hare" className="hidden col-span-2 mt-4">
           Texto HARE
         </div>
       </div>
--- src/assets/home/titulo.js
+++ src/assets/home/titulo.js
@@ -1,20 +1,20 @@
-export default function Titulo(){
-    return(
-        <section id="titulo" class=" w-full md:w-auto md:ml-28 md:mr-14 m-auto gap-4 grid mb-40 md:mb-64">
-        <h1 class="text-5xl font-bold w-full">
-          Unidades Curriculares <br />
-          do Primeiro Semestre
-        </h1>
-        <p class="font-normal w-[80%] md:w-4/12">
-          Abaixo, você terá acesso a todas as unidades curriculares oferecidas,
-          permitindo uma visão abrangente das disciplinas que compõem nosso
-          programa educacional.
-        </p>
-        <a
-          id="btnPIntegrador"
-          href="#pIntegrador"
-          class="bg-black text-white pl-4 p-2 pr-4 w-5/12 md:w-3/12 rounded-md"
-          >Projeto Integrador</a>
-      </section>
-    )
+export default function Titulo() {
+  return (
+    <section id="titulo" class=" w-full md:w-auto md:ml-28 md:mr-14 m-auto gap-4 grid mb-40 md:mb-64">
+      <h1 class="text-5xl font-bold w-full">
+        Unidades Curriculares <br />
+        do Primeiro Semestre
+      </h1>
+      <p class="font-normal w-[80%] md:w-4/12">
+        Abaixo, você terá acesso a todas as unidades curriculares oferecidas,
+        permitindo uma visão abrangente das disciplinas que compõem nosso
+        programa educacional.
+      </p>
+      <a
+        id="btnPIntegrador"
+        href="#pIntegrador"
+        class="bg-black text-white pl-4 p-2 pr-4 w-5/12 md:w-3/12 rounded-md"
+      >Projeto Integrador</a>
+    </section>
+  )
 }
