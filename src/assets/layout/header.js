3e3fc6f6979e1354c485dd5d97216a5fcd47f00e
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-12-12 10:55:12 -0300

    Update header.js

--- src/assets/layout/header.js
+++ src/assets/layout/header.js
@@ -4,7 +4,7 @@ export default function Header () {
   return (
     <header
       id="Header"
-      class="flex text-black content-center w-11/12 p-0 m-0 md:w-10/12 bg-[white] rounded-[20px] ml-auto mr-auto m-24 shadow-2xl mt-[4vh] tex-sm md:text-xl h-14"
+      class="flex text-black content-center w-11/12 p-0 m-0 bg-[white] rounded-[20px] ml-auto mr-auto m-24 shadow-2xl mt-[4vh] tex-sm md:text-xl h-14"
     >
       <div
         id="conteudo"
