524016862355514622b54c22a17a53ac2c6e543c
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-12-12 10:25:35 -0300

    Update footer.js

--- src/assets/layout/footer.js
+++ src/assets/layout/footer.js
@@ -17,7 +17,7 @@ export default function Footer(){
         <h1 class="w-max m-auto">Criado e desenvolvido por:</h1>
       </div>
 
-      <div id="avatar-container" class="grid grid-cols-auto md:grid-cols-1 items-center justify-between">
+      <div id="avatar-container" class="grid grid-cols-5 md:grid-cols-1 items-center justify-between">
         <Devs avatar={avAnderson} name="Anderson"/>
         <Devs avatar={avDaniel} name="Daniel"/>
         <Devs avatar={avHiago} name="Hiago"/>
