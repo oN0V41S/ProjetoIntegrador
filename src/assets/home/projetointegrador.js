d537e06466eaa2d240952f2f734061eb74c05053
ON0V41S <rafaelaugustonnovais@gmail.com>
2023-12-13 17:42:01 -0300

    create list for sop

--- src/assets/home/projetointegrador.js
+++ src/assets/home/projetointegrador.js
@@ -42,56 +42,87 @@ export default function ProjetoIntegrador() {
           className="hidden mt-4 mb-4 flex bg-card2 p-5 rounded-md w-full col-span-2"
         >
           <div className="mb-8">
-            1. Baixar e Instalar o VirtualBox e a ISO do Linux Ubuntu: Baixe e
-            instale o VirtualBox do site oficial. Baixe a iso do Linux Ubuntu
-            (22.04.3 LTS) do site oficial. <br />
-            2. Configurar uma nova máquina virtual no VirtualBox: Abra o
-            VirtualBox. Clique em "Novo" para criar uma nova máquina virtual.
-            Siga o assistente para configurar a máquina virtual (nome, tipo e
-            versão do sistema operacional, memória RAM, e disco rígido virtual).
-            Na guia "Armazenamento", selecione o arquivo ISO do Ubuntu que você
-            baixou. <br />
-            3. Instalar o Ubuntu na máquina virtual: Inicie a máquina virtual.
-            Siga as instruções na tela para instalar o Ubuntu. <br />
-            4. Configurar o Ubuntu na máquina virtual: Conclua a instalação do
-            Ubuntu, incluindo a criação de um usuário e senha. 5. Instalação do
-            Oracle JDK via Arquivo .deb: <br />
-            5.1 Acesse o Site da Oracle: Abra seu navegador e vá para a página
-            de downloads da Oracle. <br />
-            5.2. Download do JDK: Na seção "JDK Download", escolha a versão do
-            JDK que deseja instalar (baixe a última versão ou a que for
-            recomendada). Aceite os termos de uso e clique no botão de download
-            adequado ao seu sistema operacional. <br />
-            5.3. Aceitar os Termos de Licença: Após o download, você pode ser
-            redirecionado para uma página onde precisa aceitar os termos de
-            licença da Oracle. Faça isso para prosseguir. <br />
-            5.4. Baixar o Arquivo .deb e instale: Na página de download real,
-            escolha a versão do JDK e do sistema operacional Linux específicos.
-            Baixe o arquivo .deb correspondente. Após baixar, instale em sua
-            máquina via “Ubuntu Software”. <br />
-            6. Instalar o Visual Studio Code no Ubuntu: No Ubuntu, abra a "Loja
-            de Aplicativos" (Ubuntu Software). Pesquise por "Visual Studio
-            Code". Selecione o Visual Studio Code na lista e clique em
-            "Instalar". <br />
-            7. Abrir o Visual Studio Code: Após a instalação, abra o Visual
-            Studio Code a partir do menu de aplicativos. <br />
-            7.1 . Instalar o Extension Pack do Java: Abra o Visual Studio Code e
-            vá para a seção de extensões (clique no ícone de quadrado no painel
-            lateral esquerdo ou pressione Ctrl+Shift+X). Pesquise por "Java
-            Extension Pack" e instale a extensão que é fornecida pela Microsoft.{" "}
-            <br />
-            7.2. Criar ou Abrir um Projeto Java: Crie um novo projeto Java ou
-            abra um projeto existente no Visual Studio Code. <br />
-            7.3. Instalar a Extensão Debugger for Java: No Visual Studio Code,
-            vá para a seção de extensões, pesquise por "Debugger for Java" e
-            instale a extensão. <br />
-            7.4. Executar e Depurar o Código: Abra um arquivo Java no Visual
-            Studio Code, configure breakpoints conforme necessário e execute ou
-            depure seu código usando as opções na barra lateral ou pressionando
-            F5 para depuração. Com esses passos, você deve ter configurado o
-            Visual Studio Code para o desenvolvimento em Java na sua máquina
-            virtual Ubuntu. Lembre-se de ajustar os caminhos conforme
-            necessário, dependendo da versão do JDK que você instalou.
+            <ul className="grid gap-4 md:gap-2">
+              <li>
+                1. Baixar e Instalar o VirtualBox e a ISO do Linux Ubuntu: Baixe
+                e instale o VirtualBox do site oficial. Baixe a iso do Linux
+                Ubuntu (22.04.3 LTS) do site oficial.
+              </li>
+              <li>
+                2. Configurar uma nova máquina virtual no VirtualBox: Abra o
+                VirtualBox. Clique em "Novo" para criar uma nova máquina
+                virtual. Siga o assistente para configurar a máquina virtual
+                (nome, tipo e versão do sistema operacional, memória RAM, e
+                disco rígido virtual). Na guia "Armazenamento", selecione o
+                arquivo ISO do Ubuntu que você baixou.
+              </li>
+              <li>
+                3. Instalar o Ubuntu na máquina virtual: Inicie a máquina
+                virtual. Siga as instruções na tela para instalar o Ubuntu.
+              </li>
+              <li>
+                4. Configurar o Ubuntu na máquina virtual: Conclua a instalação
+                do Ubuntu, incluindo a criação de um usuário e senha. 5.
+                Instalação do Oracle JDK via Arquivo .deb:
+              </li>
+              <li>
+                5.1 Acesse o Site da Oracle: Abra seu navegador e vá para a
+                página de downloads da Oracle.
+              </li>
+              <li>
+                5.2. Download do JDK: Na seção "JDK Download", escolha a versão
+                do JDK que deseja instalar (baixe a última versão ou a que for
+                recomendada). Aceite os termos de uso e clique no botão de
+                download adequado ao seu sistema operacional.
+              </li>
+              <li>
+                5.3. Aceitar os Termos de Licença: Após o download, você pode
+                ser redirecionado para uma página onde precisa aceitar os termos
+                de licença da Oracle. Faça isso para prosseguir.
+              </li>
+              <li>
+                5.4. Baixar o Arquivo .deb e instale: Na página de download
+                real, escolha a versão do JDK e do sistema operacional Linux
+                específicos. Baixe o arquivo .deb correspondente. Após baixar,
+                instale em sua máquina via “Ubuntu Software”.
+              </li>
+              <li>
+                6. Instalar o Visual Studio Code no Ubuntu: No Ubuntu, abra a
+                "Loja de Aplicativos" (Ubuntu Software). Pesquise por "Visual
+                Studio Code". Selecione o Visual Studio Code na lista e clique
+                em "Instalar".
+              </li>
+              <li>
+                7. Abrir o Visual Studio Code: Após a instalação, abra o Visual
+                Studio Code a partir do menu de aplicativos.
+              </li>
+              <li>
+                7.1 . Instalar o Extension Pack do Java: Abra o Visual Studio
+                Code e vá para a seção de extensões (clique no ícone de quadrado
+                no painel lateral esquerdo ou pressione Ctrl+Shift+X). Pesquise
+                por "Java Extension Pack" e instale a extensão que é fornecida
+                pela Microsoft.
+              </li>
+              <li>
+                7.2. Criar ou Abrir um Projeto Java: Crie um novo projeto Java
+                ou abra um projeto existente no Visual Studio Code.
+              </li>
+              <li>
+                7.3. Instalar a Extensão Debugger for Java: No Visual Studio
+                Code, vá para a seção de extensões, pesquise por "Debugger for
+                Java" e instale a extensão.
+              </li>
+              <li>
+                7.4. Executar e Depurar o Código: Abra um arquivo Java no Visual
+                Studio Code, configure breakpoints conforme necessário e execute
+                ou depure seu código usando as opções na barra lateral ou
+                pressionando F5 para depuração. Com esses passos, você deve ter
+                configurado o Visual Studio Code para o desenvolvimento em Java
+                na sua máquina virtual Ubuntu. Lembre-se de ajustar os caminhos
+                conforme necessário, dependendo da versão do JDK que você
+                instalou.
+              </li>
+            </ul>
           </div>
           <video src={videoSOP} controls autoplay className="w-full" />
         </div>
