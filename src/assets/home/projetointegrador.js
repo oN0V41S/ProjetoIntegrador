50306e18dbe2705574cb1f8cdebe70f4ebe3e795
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-12-14 08:22:23 -0300

    Update projetointegrador.js

--- src/assets/home/projetointegrador.js
+++ src/assets/home/projetointegrador.js
@@ -19,7 +19,7 @@ export default function ProjetoIntegrador() {
         />
         <label
           for="btnHare"
-          className="h-max w-max bg-card2 p-2 m-auto rounded-md "
+          className="h-max w-max bg-card2 p-2 m-auto rounded-md pl-4 pr-4"
         >
           HARE
         </label>
@@ -33,7 +33,7 @@ export default function ProjetoIntegrador() {
         />
         <label
           for="btnSop"
-          className="h-max w-max bg-card2 p-2 m-auto rounded-md"
+          className="h-max w-max bg-card2 p-2 m-auto rounded-md pl-4 pr-4"
         >
           SOP
         </label>
